24bc3babf736829517dfff22275eeac921569447
takeyarise <take1010ct@gmail.com>
2024-10-09 10:12:04 +0900

    torch.compile に対応

--- common/optim/sam.py
+++ common/optim/sam.py
@@ -29,7 +29,7 @@ def bypass_running_stats(model):
         if isinstance(module, _BatchNorm):
             module.eval()
     def _enable(module):
-        if isinstance(module, _BatchNorm) and hasattr(module, "backup_training"):
+        if isinstance(module, _BatchNorm):
             module.train()
     model.apply(_disable)
     try:
@@ -41,34 +41,6 @@ def bypass_running_stats(model):
 class SAM(torch.optim.Optimizer):
     # src: https://github.com/davda54/sam
     def __init__(self, params, base_optimizer, rho=0.05, adaptive=False, **kwargs):
-        """__init__ _summary_
-
-        Parameters
-        ----------
-        params : _type_
-            _description_
-        base_optimizer : _type_
-            _description_
-        rho : float, optional
-            _description_, by default 0.05
-        adaptive : bool, optional
-            _description_, by default False
-
-        Example
-        -------
-        >>> for batch in dataset.train:
-        >>>   inputs, targets = (b.to(device) for b in batch)
-        >>>   # first forward-backward step
-        >>>   enable_running_stats(model)  # <- this is the important line if you use batch normalization
-        >>>   predictions = model(inputs)
-        >>>   loss = smooth_crossentropy(predictions, targets)
-        >>>   loss.mean().backward()
-        >>>   optimizer.first_step(zero_grad=True)
-        >>>   # second forward-backward step
-        >>>   disable_running_stats(model)  # <- this is the important line if you use batch normalization
-        >>>   smooth_crossentropy(model(inputs), targets).mean().backward()
-        >>>   optimizer.second_step(zero_grad=True)
-        """
         assert rho >= 0.0, f"Invalid rho, should be non-negative: {rho}"
 
         defaults = dict(rho=rho, adaptive=adaptive, **kwargs)
@@ -85,23 +57,27 @@ class SAM(torch.optim.Optimizer):
             scale = group["rho"] / (grad_norm + 1e-12)
 
             for p in group["params"]:
-                if p.grad is None: continue
-                self.state[p]["old_p"] = p.data.clone()
+                if p.grad is None:
+                    continue
+                self.state[p]["old_p"] = p.clone()
                 e_w = (torch.pow(p, 2) if group["adaptive"] else 1.0) * p.grad * scale.to(p)
                 p.add_(e_w)  # climb to the local maximum "w + e(w)"
 
-        if zero_grad: self.zero_grad()
+        if zero_grad:
+            self.zero_grad()
 
     @torch.no_grad()
     def second_step(self, zero_grad=False):
         for group in self.param_groups:
             for p in group["params"]:
-                if p.grad is None: continue
-                p.data = self.state[p]["old_p"]  # get back to "w" from "w + e(w)"
+                if p.grad is None:
+                    continue
+                p.copy_(self.state[p]["old_p"])  # get back to "w" from "w + e(w)"
 
         self.base_optimizer.step()  # do the actual "sharpness-aware" update
 
-        if zero_grad: self.zero_grad()
+        if zero_grad:
+            self.zero_grad()
 
     @torch.no_grad()
     def step(self, closure=None):
@@ -115,13 +91,13 @@ class SAM(torch.optim.Optimizer):
     def _grad_norm(self):
         shared_device = self.param_groups[0]["params"][0].device  # put everything on the same device, in case of model parallelism
         norm = torch.norm(
-                    torch.stack([
-                        ((torch.abs(p) if group["adaptive"] else 1.0) * p.grad).norm(p=2).to(shared_device)
-                        for group in self.param_groups for p in group["params"]
-                        if p.grad is not None
-                    ]),
-                    p=2
-               )
+            torch.stack([
+                ((torch.abs(p) if group["adaptive"] else 1.0) * p.grad).norm(p=2).to(shared_device)
+                for group in self.param_groups for p in group["params"]
+                if p.grad is not None
+            ]),
+            p=2
+        )
         return norm
 
     def load_state_dict(self, state_dict):
